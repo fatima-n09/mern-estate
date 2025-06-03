e80fdcc0bf116c254a2cf47c5c8c02e6220ff287
Fatima Nur <fatimanur424@example.com>
2025-06-04 01:51:11 +0500

    create pages and route

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import React from "react";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { About } from "./pages/About";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import { SignUp } from "./pages/SignUp";
+import Home from "./pages/Home";
 function App() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <h1 className="text-4xl font-bold text-blue-600">
-        Hello, Tailwind CSS in React!
-      </h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 

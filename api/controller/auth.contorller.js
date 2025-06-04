d8b4669f644dbe889fb3ddca453cb615ba9576e9
Fatima Nur <fatimanur424@example.com>
2025-06-05 01:26:30 +0500

    create signIn PI route

--- api/controller/auth.contorller.js
+++ api/controller/auth.contorller.js
@@ -1,6 +1,6 @@
 import User from "../models/User.model.js";
 import bcrypt from "bcryptjs";
-
+import jwt from "jsonwebtoken";
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
   const hashedPassword = bcrypt.hashSync(password, 10);
@@ -12,6 +12,26 @@ export const signup = async (req, res, next) => {
   try {
     await newUser.save();
     res.status(201).json("User created successfully");
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signIn = async (req, res, next) =>{
+  const {email, password} = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    const isMatch = bcrypt.compareSync(password, user.password);
+    if (!isMatch) {
+      return res.status(401).json({ message: "Invalid credentials" });
+    }
+    const token = jwt.sign({id : user._id}, process.env.JWT_SECRET);
+    const {password: pass, ...userData} = user._doc;
+    res.cookie("access_token", token, { httpOnly: true });
+    res.status(200).json({ message: "Sign in successful", user: userData });
   } catch (error) {
     next(error);
   }
--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,7 +1,8 @@
 import express from 'express';
-import { signup } from '../controller/auth.contorller.js';
+import { signup, signIn } from '../controller/auth.contorller.js';
 
 const router = express.Router();
 
 router.post('/signup', signup);
+router.post('/signin', signIn);
 export default router;

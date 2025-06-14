38af19905ce0927d3f021408b3e0da0bcd0340d4
Fatima Nur <fatimanur424@example.com>
2025-06-15 02:26:28 +0500

    complete update user functionality

--- api/utels/User.js
+++ api/utels/User.js
@@ -1,10 +1,13 @@
-export const verifyToken = ( req, res, next) => {
+import Jwt from 'jsonwebtoken';
+import {errorHandler} from './error.js';
+
+export const verifyToken = (req, res, next) => {
     const token = req.cookies.access_token;
     if(!token) return res.status(401).json({ message: "Unauthorized" });
 
-    Jwt.verifyToken(token, process.env.JWT_SECRET, (err, user) => {
-        if (err) return next(errorHandles(403, 'Forbidden'));
+    Jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+        if (err) return next(errorHandler(403, 'Forbidden'));
         req.user = user;
         next();
-    })
-}
+    });
+};
--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,85 +1,81 @@
-import { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
 import {
   signInStart,
   signInSuccess,
   signInFailure,
-} from "../redux/User/User";
-import { Oauth } from "../components/Oauth";
-export const SignIn = () => {
-  const [formData, setFormData] = useState({
-    email: "",
-    password: "",
-  });
+} from '../redux/User/User';
+import OAuth from '../components/OAuth';
 
-  const { loading, error} = useSelector((state) => state.user);
+export default function SignIn() {
+  const [formData, setFormData] = useState({});
+  const { loading, error } = useSelector((state) => state.user);
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const handleChange = (e) => {
-    setFormData({ ...formData, [e.target.id]: e.target.value });
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
   };
-
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
       dispatch(signInStart());
-
-      const res = await fetch("/api/auth/signin", {
-        method: "POST",
+      const res = await fetch('/api/auth/signin', {
+        method: 'POST',
         headers: {
-          "Content-type": "application/json",
+          'Content-Type': 'application/json',
         },
         body: JSON.stringify(formData),
       });
-
       const data = await res.json();
-
+      console.log(data);
       if (data.success === false) {
         dispatch(signInFailure(data.message));
         return;
       }
-
-      dispatch(signInSuccess(data.user));
-      navigate("/");
+      dispatch(signInSuccess(data));
+      navigate('/');
     } catch (error) {
-      dispatch(signInFailure(error.message || "Something went wrong"));
+      dispatch(signInFailure(error.message));
     }
   };
-
   return (
-    <div className="p-3 max-w-lg mx-auto">
-      <h1 className="text-3xl text-center font-semibold my-7">Sign In</h1>
-      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+    <div className='p-3 max-w-lg mx-auto'>
+      <h1 className='text-3xl text-center font-semibold my-7'>Sign In</h1>
+      <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
         <input
-          type="email"
-          placeholder="Email"
-          className="border p-3 rounded-lg"
-          id="email"
+          type='email'
+          placeholder='email'
+          className='border p-3 rounded-lg'
+          id='email'
           onChange={handleChange}
         />
         <input
-          type="password"
-          placeholder="Password"
-          className="border p-3 rounded-lg"
-          id="password"
+          type='password'
+          placeholder='password'
+          className='border p-3 rounded-lg'
+          id='password'
           onChange={handleChange}
         />
+
         <button
           disabled={loading}
-          className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
+          className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'
         >
-          {loading ? "Loading..." : "Sign In"}
+          {loading ? 'Loading...' : 'Sign In'}
         </button>
-        <Oauth/>
+        <OAuth/>
       </form>
-      <div className="flex gap-2 mt-5">
-        <p>Don't have an account?</p>
-        <Link to="/signup">
-          <span className="text-blue-500">Sign Up</span>
+      <div className='flex gap-2 mt-5'>
+        <p>Dont have an account?</p>
+        <Link to={'/sign-up'}>
+          <span className='text-blue-700'>Sign up</span>
         </Link>
       </div>
-      {error && <p className="text-red-500 mt-2">{error}</p>}
+      {error && <p className='text-red-900 mt-5'>{error}</p>}
     </div>
   );
-};
+}

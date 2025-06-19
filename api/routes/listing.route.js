655c1b66090bcf456cde76b771ac0551f1d93615
Fatima Nur <fatimanur424@example.com>
2025-06-19 13:09:39 +0500

    get route

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import { verifyToken } from '../utels/User.js';
-import { createListing, deleteListing, updateListing } from '../controller/listing.controller.js';
+import { createListing, deleteListing, getListing, updateListing } from '../controller/listing.controller.js';
 
 const router = express.Router();
  
 router.post('/create', verifyToken, createListing)
 router.delete('/delete/:id', verifyToken, deleteListing)
 router.post('/update/:id', verifyToken, updateListing)
+router.get('/get/:id', getListing)
 export default router;
